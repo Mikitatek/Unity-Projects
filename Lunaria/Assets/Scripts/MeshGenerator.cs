0824706052fd7d886dc0f3f024405eade1227083
Mikitatek <mikitatek@gmail.com>
2022-05-15 16:36:59 +0300

    Infinite/Island Procedural Maps
    EP12 normals

--- Lunaria/Assets/Scripts/MeshGenerator.cs
+++ Lunaria/Assets/Scripts/MeshGenerator.cs
@@ -3,7 +3,8 @@ using System.Collections;
 
 public static class MeshGenerator {
 
-	public static MeshData GenerateTerrainMesh(float[,] heightMap, float heightMultiplier, AnimationCurve heightCurve, int levelOfDetail) {
+	public static MeshData GenerateTerrainMesh(float[,] heightMap, float heightMultiplier, AnimationCurve _heightCurve, int levelOfDetail) {
+		AnimationCurve heightCurve = new AnimationCurve(_heightCurve.keys);
 		int width = heightMap.GetLength (0);
 		int height = heightMap.GetLength (1);
 		float topLeftX = (width - 1) / -2f;
@@ -45,14 +46,50 @@ public class MeshData {
 	public MeshData(int meshWidth, int meshHeight) {
 		vertices = new Vector3[meshWidth * meshHeight];
 		uvs = new Vector2[meshWidth * meshHeight];
-		triangles = new int[(meshWidth-1)*(meshHeight-1)*6];
+		triangles = new int[(meshWidth - 1) * (meshHeight - 1) * 6];
 	}
 
 	public void AddTriangle(int a, int b, int c) {
-		triangles [triangleIndex] = a;
-		triangles [triangleIndex + 1] = b;
-		triangles [triangleIndex + 2] = c;
+		triangles[triangleIndex] = a;
+		triangles[triangleIndex + 1] = b;
+		triangles[triangleIndex + 2] = c;
 		triangleIndex += 3;
+	}
+
+	Vector3[] CalculateNormals()
+	{
+		Vector3[] vertexNormals = new Vector3[vertices.Length];
+		int triangleCount = triangles.Length / 3;
+		for (int i = 0; i < triangleCount; i++)
+		{
+			int normalTriangleIndex = i * 3;
+			int vertexIndexA = triangles[normalTriangleIndex];
+			int vertexIndexB = triangles[normalTriangleIndex + 1];
+			int vertexIndexC = triangles[normalTriangleIndex + 2];
+
+			Vector3 triangleNormal = SurfaceNormalFromIndices(vertexIndexA, vertexIndexB, vertexIndexC);
+			vertexNormals[vertexIndexA] += triangleNormal;
+			vertexNormals[vertexIndexB] += triangleNormal;
+			vertexNormals[vertexIndexC] += triangleNormal;
+		}
+
+		for(int i = 0; i < vertexNormals.Length; i++)
+        {
+			vertexNormals[i].Normalize();
+        }
+
+		return vertexNormals;
+
+	}
+
+	Vector3 SurfaceNormalFromIndices(int indexA, int indexB, int indexC) {
+		Vector3 pointA = vertices[indexA];
+		Vector3 pointB = vertices[indexB];
+		Vector3 pointC = vertices[indexC];
+
+		Vector3 sideAB = pointB - pointA;
+		Vector3 sideAC = pointC - pointA;
+		return Vector3.Cross(sideAB, sideAC).normalized;
 	}
 
 	public Mesh CreateMesh() {
@@ -60,7 +97,7 @@ public class MeshData {
 		mesh.vertices = vertices;
 		mesh.triangles = triangles;
 		mesh.uv = uvs;
-		mesh.RecalculateNormals ();
+		mesh.normals = CalculateNormals();
 		return mesh;
 	}
 
